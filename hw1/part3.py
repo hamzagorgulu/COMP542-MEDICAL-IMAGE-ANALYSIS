43bbfdfd69eb33d1c7c629350ea91e88336bed54
unknown <herzbasti97@gmail.com>
2023-04-18 14:44:52 +0300

    part3 ready for full pipeline

--- hw1/part3.py
+++ hw1/part3.py
@@ -1,19 +1,36 @@
 import cv2
 import numpy as np
-from utils import show, load, readtxt, get_cells_as_singel_objects, get_seeds, get_mean_of_cell, region_grow
+from utils import show, load, readtxt, get_cells_as_singel_objects, get_seeds, get_mean_of_cell, region_grow, filtered_connected_components, evaluate_cells, fuse_predictions_to_print
 
-def segment(img, mask, seed, cell): #Need to initialize in middle of cell and mean of cell intensitiy does not work
-    cell_idx = 5
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    blured = cv2.GaussianBlur(gray, (5,5), 0)
-    #mean_of_cell = get_mean_of_cell(blured, cell[cell_idx])
-    out = region_grow(blured, seed[cell_idx][0], seed[cell_idx][1], 23)
-    out[out < 1] = 0
-    out = out.astype(np.uint8)
-    cell[cell_idx] = cell[cell_idx].astype(np.uint8)
-    show([blured, out, cell[cell_idx]])
 
-    pass
+def segment(img, mask, seed, cell):
+    segments = []
+    for cell_idx in range(len(seed)):
+        print(cell_idx)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        blured = cv2.medianBlur(gray, 3)
+        out = region_grow(blured, seed[cell_idx]
+                          [0], seed[cell_idx][1], 0.38, mask)
+        out[out < 1] = 0
+        out = out.astype(np.uint8)
+        cell[cell_idx] = cell[cell_idx].astype(np.uint8)
+        if np.sum(out) > 800:
+            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+            out = cv2.morphologyEx(out, cv2.MORPH_ERODE, kernel, iterations=2)
+            out = filtered_connected_components(out, 8, 1, seed[cell_idx])
+            out = cv2.morphologyEx(out, cv2.MORPH_DILATE, kernel, iterations=2)
+        out = cv2.morphologyEx(out, cv2.MORPH_CLOSE, cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE, (7, 7)), iterations=1)
+        out = cv2.morphologyEx(out, cv2.MORPH_DILATE, cv2.getStructuringElement(
+            cv2.MORPH_ELLIPSE, (5, 5)), iterations=1)
+        #show([blured, out, cell[cell_idx]])
+        segments.append(out)
+    map = fuse_predictions_to_print(segments)
+    cell_map = fuse_predictions_to_print(cell)
+    show([img, map, cell_map.astype(np.uint8)])
+    return segments
+
+
 
 def main():
     img1_path = "data\\im1.jpg"
@@ -40,25 +57,31 @@ def main():
         imgs.append(load(img, 1))
         masks.append(readtxt(mask))
         cells.append(readtxt(cell))
-        
+
     # artificially create cell locations
-    for cell in cells:
+    for img, cell in zip(imgs, cells):
         cell_object = get_cells_as_singel_objects(cell)
         cell_objects.append(cell_object)
-        seed_objects = get_seeds(cell_object)
+
+        seed_objects = get_seeds(cell_object, img)
         seeds.append(seed_objects)
         for i in range(len(cell_object)):
             if cell_object[i][seed_objects[i][0], seed_objects[i][1]] != 1:
                 print("Seed and cell does not match")
 
-    for img, mask, seed, cell in zip(imgs, masks, seeds, cell_objects): # for debugging)
+    # for debugging)
+    for img, mask, seed, cell in zip(imgs, masks, seeds, cell_objects):
         output = segment(img, mask, seed, cell)
         outputs.append(output)
-        show(img, output)
+        
+        
 
-    # for mask, gold in zip(masks, golds):
-    #     score = evaluate(mask, gold)
-    #     print(score)
+    for img, out, cell in zip(imgs, outputs, cell_objects):
+        score = evaluate_cells(out, cell)
+        print(score)
+        map = fuse_predictions_to_print(out)
+        show([map])
+        
 
 
 if __name__ == "__main__":
--- hw1/utils.py
+++ hw1/utils.py
@@ -11,12 +11,17 @@ def get_cells_as_singel_objects(cells):
     cell_objects = [(cells == i+1)*1 for i in range(int(np.max(cells)))]
     return cell_objects
 
-def get_seeds(cells):
+def get_seeds(cells, img):
     seeds = []
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(3,3))
     
     for cell in cells:
         coord = []
-        argmax = np.unravel_index(cell.argmax(), cell.shape)
+        cell = cv2.morphologyEx(cell.astype(np.uint8), cv2.MORPH_ERODE, kernel, iterations=5)
+        activations = cell * cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        activations[activations==0] = 255
+        argmin = np.unravel_index(activations.argmin(), activations.shape)
+        #argmax = np.unravel_index(cell.argmax(), cell.shape)
 
         # for i in range(cell.shape[0]):
         #     for j in range(cell.shape[1]):
@@ -24,10 +29,11 @@ def get_seeds(cells):
         #             coord.append([i, j])
         # cog = np.mean(coord, axis=0)
         # cog = cog.astype(np.uint16)
-        seeds.append(argmax)
+        seeds.append(argmin)
     return seeds
 
 def get_mean_of_cell(img, cell):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = img.astype(cell.dtype)
     img = img.flatten()
     cell = cell.flatten()
@@ -41,6 +47,7 @@ def show(imgs):
     cv2.namedWindow('image', cv2.WINDOW_NORMAL)
     cv2.resizeWindow('image', 800, 300)
     for i in range(len(imgs)):
+        imgs[i] = imgs[i].astype(np.uint8)
         if len(imgs[i].shape) != 3 and np.max(imgs[i]) > 1:
             imgs[i] = cv2.cvtColor(imgs[i], cv2.COLOR_GRAY2BGR)
         elif len(imgs[i].shape) != 3 and np.max(imgs[i]) <= 1:
@@ -113,7 +120,7 @@ def auto_canny(image, sigma=0.05):
     return edged
 
 
-def filtered_connected_components(img, connectivity, area_threshold):
+def filtered_connected_components(img, connectivity, area_threshold, seed=None):
     analysis = cv2.connectedComponentsWithStats(img,
                                                 connectivity,
                                                 cv2.CV_32S)
@@ -131,6 +138,9 @@ def filtered_connected_components(img, connectivity, area_threshold):
 
         if (area > area_threshold):
             componentMask = (label_ids == i).astype("uint8") * 255
+            if seed:
+                if componentMask[seed[0], seed[1]] == 0:
+                    continue
             output = cv2.bitwise_or(output, componentMask)
     return output
 
@@ -159,82 +169,93 @@ def evaluate(mask, gold):
     return {"precision": precision, "recall": recall, "fscore":fscore}
 
 def get4ngb(rows, cols, x, y):
-    # function ngb = get4ngb(rows, cols, x, y)
-    # 
-    # Input:
-    # rows, cols    Number of rows and colums from the 2D Image
-    # x,y           Coordinates of the center point
-    #
-    # Output:
-    # ngb           list of possible neighbours
-
-    # initialize empty result list
     ngb = list()
-
-    # left
     if x > 1:
         ngb += [[y, x-1]]
-
-    # down
     if y < rows-1:
         ngb += [[y+1, x]]
-
-    # right
     if x < cols-1:
         ngb += [[y, x+1]]
-
-    # up
     if y > 1:
         ngb += [[y-1, x]]
-
     return ngb 
-# Our similarity metric. Given two points with shape=(3,), return true or false if the distance 
-# is lower or biger than th. Use the l1 distance (mean of absolute values)
+
 def is_similar(p1, p2, th):
     p1 = p1.astype(np.int16)
     p2 = p2.astype(np.int16)
-    # TODO: your metric here.
     dist = np.mean(np.abs(p1 - p2))
-    return dist < th
+    max_dist = (255 - np.mean(p2))*th
+    return dist < max_dist
 
 
-# The algorithm. Our root is the pixel in position (seedX; seedY)
-def region_grow(I, seedY, seedX, th):
+def region_grow(I, seedY, seedX, th, mask):
     if len(I.shape) == 3:
       rows, cols, ch = I.shape
     else:
       rows, cols = I.shape
-    # our map for the nodes (pixels) that we have not discovered yet (-1),
-    # we have discovered (0)
-    # we have discovered and meet the similarity condition (1). This is our segmentation
+    mask = 1 - mask
     R = -np.ones(shape=(rows, cols))
+    R = R + mask
     queue = []
-    # enqueue the root
     queue.append([seedY, seedX])
     
-    # Setting the root as discovered
     R[seedY, seedX] = 0
-    
-    #While our queue is not empty
     while len(queue) > 0:
-        # we dequeue the first node of the queue
         v = queue[0]
         queue = queue[1:]
         
-        # TODO: Use our similarity function to check the condition on the node v
         if is_similar(I[v[0], v[1]], I[seedY, seedX], th): # 
-            # TODO: set the node v as "meet the condition", that is, R[vy, vx] = 1
             R[v[0], v[1]] = 1
-            # TODO: determine neighbors
             nbgs = get4ngb(rows, cols, v[1], v[0])
-            # for all the neighbors of v
             for i in range(0,len(nbgs)):
                 qq = nbgs[i]
-                # If it was not discovered yet:
                 if (R[qq[0], qq[1]] < 0):
-                    #TODO: label as discovered
                     R[qq[0], qq[1]] = 0
-                    #TODO: enqueue it 
                     queue.append([qq[0], qq[1]])
             
     return R
+
+def IoU(pred, target):
+    pred[pred > 0] = 1
+    pred[target > 0] = 1
+    TP = np.sum(np.logical_and(pred == 1, target == 1))
+    TN = np.sum(np.logical_and(pred == 0, target == 0))
+    FP = np.sum(np.logical_and(pred == 1, target == 0))
+    FN = np.sum(np.logical_and(pred == 0, target == 1))
+    return TP/(TP + FP + FN)
+
+def evaluate_cells(predictions, targets):
+    results = [] # dice0.5, iou0.5 dice0.75,iou0.75, dice0.9, iou0.9
+    thresholds = [0.5, 0.75, 0.9]
+    for thr in thresholds:
+        TP = 0
+        FP = 0
+        FN = 0
+        for i, pred in enumerate(predictions):
+            print("pred", i)
+            max_iou = 0
+            for target in targets:
+                if IoU(pred, target) > max_iou:
+                    max_iou = IoU(pred, target)
+            if max_iou > thr:
+                TP += 1
+            else:
+                FP += 1
+
+        FN = len(targets) - (TP + FP)
+        dice = 2*TP / (2*TP + FP + FN)
+        iou = TP / (TP + FP + FN)
+        results.append(dice)
+        results.append(iou)
+    return results
+        
+        
+            
+def fuse_predictions_to_print(predictions):
+    output = np.zeros(predictions[0].shape + (3,), dtype="uint8")
+    for pred in predictions:
+        pred = cv2.cvtColor(pred, cv2.COLOR_GRAY2BGR)
+        color = list(np.random.random(size=3) * 256)
+        pred = pred * color
+        output = output + pred
+    return output
